cfebc5de51907d2bff9d8d1953d5f3a21f11323e
luckboyi <546217953@qq.com>
2019-03-11 09:42:58 +0800

    vue

---
+++ commonVue.js
@@ -0,0 +1,10 @@
+exports.install = function(Vue,options) {
+  Vue.prototype.addFn = function(model,data,callbackFn) {
+    console.log(model)
+    console.log(data)
+    console.log(callbackFn)
+  }
+}
+//在main.js中
+//import commonVue from './commonVue
+//Vue.use(commonVue)
